5fca1f3bc73a71f794bb7a11fe88e2d0fe4e86ce
Patricio Balardini <patriciobalardini@gmail.com>
2023-08-01 00:08:31 +0800

    build: app creation

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import { Login } from "../header/Login";
+import "./header.css";
+
+export const Header = () => {
+  return (
+    <>
+      <header>
+        Header
+        <div>
+          <Login />
+        </div>
+      </header>
+    </>
+  );
+};
---
+++ src/components/header/Login.jsx
@@ -0,0 +1,9 @@
+import "./login.css";
+
+export const Login = () => {
+  return (
+    <>
+      <button>Login</button>
+    </>
+  );
+};
