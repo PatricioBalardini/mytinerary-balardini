61ca5b8532231ff00cd493c421b323b7dc2cbdf9
Patricio Balardini <patriciobalardini@gmail.com>
2023-09-13 23:50:49 +0800

    feat: complet proyect

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,6 +1,13 @@
 /* eslint-disable react/prop-types */
+import { FiLogOut } from "react-icons/fi";
 import SecondaryButton from "../buttons/SecondaryButton";
+import jwtDecode from "jwt-decode";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import userActions from "../../redux/actions/userActions";
+import { useEffect, useState } from "react";
+
+const { signOut } = userActions;
 
 const links = [
   { id: "home", label: "Home", href: "/home" },
@@ -17,12 +24,42 @@ const NavBarLink = ({ label, href }) => {
 
 export default function NavBar() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const token = localStorage.getItem("token");
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    if (token) {
+      const userStorage = jwtDecode(token);
+      setUser(userStorage);
+    }
+  }, [token]);
+
+  console.log(user);
+
+  const handleLogout = async () => {
+    dispatch(signOut())
+      .then(() => {
+        setUser(undefined);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <nav className="navbar">
       {links.map((link) => (
         <NavBarLink key={link.id} label={link.label} href={link.href} />
       ))}
-      <SecondaryButton label="Login" onClick={() => navigate("/sign-in")} />
+      {user ? (
+        <div className="navbar-user">
+          <p>{user.name}</p>
+          <button onClick={handleLogout}>
+            <FiLogOut />
+          </button>
+        </div>
+      ) : (
+        <SecondaryButton label="Login" onClick={() => navigate("/sign-in")} />
+      )}
     </nav>
   );
 }
